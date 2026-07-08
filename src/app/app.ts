d49c9e5e84817273e0d74ae404fb5e180ed4ccb3
Thangarasu Selvam <thangarasuselvam96@gmail.com>
2026-07-09 01:19:25 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Services } from './components/services/services';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Home, About, Services, Projects, Contact, Footer
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('thangarasu-portfolio');
+}
